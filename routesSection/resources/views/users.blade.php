284ed4c39768d419b869fc20b0290c84d3e2391b
yuuukiDev <mohamedemadeldin34@gmail.com>
2023-11-03 14:38:12 +0200

    updated project

---
+++ routesSection/resources/views/users.blade.php
@@ -0,0 +1,4 @@
+<form action="{{ id }}" method="post">
+    <input type="text" placeholder="Username">
+    <button type="submit">Submit</button>
+</form>
--- routesSection/routes/web.php
+++ routesSection/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/users', function(){
+    return view('users');
+});
+
+Route::get('/users/{id}', function(int $id){
+    if ($id == 1) {
+        return "admin";
+    } else {
+        return "user";
+    }
+});
