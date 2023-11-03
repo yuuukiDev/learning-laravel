a01136aac52f926370df994986ead351c3d08185
yuuukiDev <mohamedemadeldin34@gmail.com>
2023-11-03 18:33:04 +0200

    finished migration

--- migrationSection/database/migrations/2023_11_03_152421_create_posts_table.php
+++ migrationSection/database/migrations/2023_11_03_152421_create_posts_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->boolean('check')->default(0);
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->boolean('check')->default(0);;
             $table->timestamps();
         });
     }
