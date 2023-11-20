e26bfad4833022f0177d8c187f8d8cf1d4c273ac
yuuukiDev <mohamedemadeldin34@gmail.com>
2023-11-20 10:40:19 +0200

    Finished

--- seederSection/database/seeders/DatabaseSeeder.php
+++ seederSection/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,14 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
+        Post::factory(5)->create();
+        User::truncate();
+        User::factory(20)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-            $this->call([
-                UserSeeder::class,
-            ]);
+            // $this->call([
+            //     UserSeeder::class,
+            // ]);
     }
 }
