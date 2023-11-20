c7145f4f152c1f8317fa75e0e0ef12d1edb2da27
yuuukiDev <mohamedemadeldin34@gmail.com>
2023-11-20 10:10:10 +0200

    Updated

---
+++ seederSection/database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => "Mohamed",
+            'email' => "Mohamed@gmail.com",
+            'password' => Hash::make('123456'),
+        ]);
+
+        User::create([
+            'name' => "Mahmoud",
+            'email' => "Mahmoud@gmail.com",
+            'password' => Hash::make('123456'),
+        ]);
+    }
+}
