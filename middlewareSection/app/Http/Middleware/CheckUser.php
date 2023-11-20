8331622a29a23d94a92cc25da020f8bab7c03b85
yuuukiDev <mohamedemadeldin34@gmail.com>
2023-11-20 07:37:38 +0200

    Finished Project

---
+++ middlewareSection/app/Http/Middleware/CheckUser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->user()->status == 0) {
+            return response("Not active");
+        }
+        return $next($request);
+    }
+}
--- middlewareSection/app/Models/User.php
+++ middlewareSection/app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function setNameAttribute($value) {
+        $this->attributes['name'] = ucfirst($value);
+    }
 }
